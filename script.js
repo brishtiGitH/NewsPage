c9316bf5c4f1b238e9ee4064a247eac1dbd90c2d
Brishti Das <122745392+brishtiGitH@users.noreply.github.com>
2023-10-11 20:07:27 +0530

    Update script.js
    Added page overlay

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-  const showMenu = document.getElementById('menuIcon');
-    const mobileMenu = document.getElementById('mobileMenu');
-    const hideMenu = document.querySelector('#mobileMenu img');
+const showMenu = document.getElementById('menuIcon');
+const mobileMenu = document.getElementById('mobileMenu');
+const hideMenu = document.querySelector('#mobileMenu img');
+const overlay = document.getElementById('over');
+
     showMenu.addEventListener('click', () => {
       mobileMenu.style.display = 'block';
+      overlay.style.display = 'block';
+
     });
     hideMenu.addEventListener('click', () => {
       mobileMenu.style.display = 'none';
+      overlay.style.display = 'none';
     });
